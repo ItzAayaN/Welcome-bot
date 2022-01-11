97b2ca186e9d1121bd54b29502a6753c8c064580
Ayan <138840div2@gmail.com>
2022-01-11 08:22:26 +0300

    Update

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const Discord = require("discord.js")
-require("dotenv").config()
+;const Discord = require("discord.js");
+require("dotenv").config();
 
-const generateImage = require("./generateImage")
-require("status.js")
+const generateImage = require("./generateImage");
+require("status.js");
+
+if(context.params.event.content.includes("!status")){
+    
+    await lib.discord.users['@0.2.0'].me.status.update({
+       activity_name: `Server Joins`,
+       activity_type: 'WATCHING',
+       status: 'DND'
+     });
+
+     return lib.discord.channels['@0.2.0'].message.create({
+         channel_id: `${content.params.event.channel_id}`,
+         content: `Success Fully Watching`
+     });
+}
+
 
 const client = new Discord.Client({
     intents: [
@@ -10,17 +25,17 @@ const client = new Discord.Client({
         "GUILD_MESSAGES",
         "GUILD_MEMBERS"
     ]
-})
+});
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
-})
+});
 
 client.on("messageCreate", (message) => {
     if (message.content == "hi"){
         message.reply("Hello UwU")
     }
-})
+});
 
 const welcomeChannelId = "926780981485129748"
 
@@ -30,6 +45,6 @@ client.on("guildMemberAdd", async (member) => {
         content: `<@${member.id}> Welcome to the server!`,
         files: [img]
     })
-})
+});
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
